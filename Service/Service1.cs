5c4579de36e5fca9fb7f35fb3bf9b291db41ff2b
HalfCentaurHalfHuman <hoang.tran.200032@student.fulbright.edu.vn>
2022-01-03 21:18:59 +0700

    First commit

---
+++ Service/Service1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Drawing;
+using ActUtlTypeLib;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace hearty_plant_service
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        //Set up
+        public ActUtlType plc = new ActUtlType();
+        public string conString = "Data Source=PHANLANKHANH\\SQLEXPRESS;Initial Catalog=Hearty Plant Example;Integrated Security=True";
+
+        protected override void OnStart(string[] args)
+        {
+            plc.ActLogicalStationNumber = 20;
+            plc.Open();
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds  
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+            plc.Close();
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service is recall at " + DateTime.Now);
+            PushToSql();
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+        
+        public void PushToSql()
+        {
+            string input = "M1";
+            int readInt;
+            //plc.ReadDeviceBlock(input, 2, out readInt);
+            plc.GetDevice(input, out readInt);
+
+            SqlConnection connection = null;
+            SqlCommand command = null;
+            try
+            {
+                connection = new SqlConnection();
+                connection = new SqlConnection(conString);
+                connection.Open();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    string q = "INSERT INTO DataRecord VALUES ('" + null + "', '" + input + "', '" + readInt.ToString() + "')";
+                    command = new SqlCommand(q, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                WriteToFile("Exception of Insert:" + ex.ToString());
+                throw ex;
+            }
+            finally
+            {
+                command.Dispose();
+                connection.Dispose();
+            }
+        }
+    }
+}  
+    
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ActUtlTypeLib;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public ActUtlType plc = new ActUtlType();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string conString = "Data Source=PHANLANKHANH\\SQLEXPRESS;Initial Catalog=Hearty Plant Example;Integrated Security=True";
+
+        //Connect button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            plc.ActLogicalStationNumber = 20;
+            plc.Open();
+
+        }
+
+        //Disconnect button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            plc.Close();
+        }
+
+        //Read button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int readInt;
+            //plc.ReadDeviceBlock(textAddress.Text, 2, out readInt);
+            plc.GetDevice("M1", out readInt);
+            textValue.Text = readInt.ToString();
+        }
+
+        //Write button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            plc.SetDevice(textAddress.Text, Convert.ToInt16(textValue.Text));
+        }
+
+        // SQL button
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = null;
+            SqlCommand command = null;
+            try
+            {
+                connection = new SqlConnection();
+                int n = 0;
+                connection = new SqlConnection(conString);
+                connection.Open();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    string q = "INSERT INTO DataRecord VALUES ('" + null + "', '" + textAddress.Text.ToString() + "', '" + textValue.Text.ToString() + "')";
+                    command = new SqlCommand(q, connection);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Connect made Successfully!");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //WriteToFile("Exception of Insert:" + ex.ToString());
+                throw ex;
+            }
+            finally
+            {
+                command.Dispose();
+                connection.Dispose();
+            }
+
+        }
+    }
+}
