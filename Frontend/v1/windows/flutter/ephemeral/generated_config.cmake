60aaf391ca3f196b91f3d0e4c4ae0cc917b066fb
HalfCentaurHalfHuman <hoang.tran.200032@student.fulbright.edu.vn>
2022-03-31 21:12:26 +0700

    Finalized API backend

---
+++ Frontend/v1/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "F:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\GDSC\\hearty-plant\\Frontend\\v1" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=F:\\flutter"
+  "PROJECT_DIR=F:\\GDSC\\hearty-plant\\Frontend\\v1"
+  "FLUTTER_ROOT=F:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\GDSC\\hearty-plant\\Frontend\\v1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\GDSC\\hearty-plant\\Frontend\\v1"
+  "FLUTTER_TARGET=F:\\GDSC\\hearty-plant\\Frontend\\v1\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\GDSC\\hearty-plant\\Frontend\\v1\\.dart_tool\\package_config.json"
+)
